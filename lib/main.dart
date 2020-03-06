a6cdb7d42eef19aac655ae6bbdd0a6b9f0d41a03
Arthur Denner <arthurdenner7@gmail.com>
2020-03-06 10:53:55 +0100

    refactor: create widget class for ColorLerps

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:color_lerp_visualizer/models/EntryColor.dart';
 import 'package:color_lerp_visualizer/widgets/ColorButton.dart';
-import 'package:color_lerp_visualizer/widgets/ColorText.dart';
+import 'package:color_lerp_visualizer/widgets/ColorLerps.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -36,43 +35,10 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-double formatDouble(double value) {
-  return double.parse(value.toStringAsFixed(2));
-}
-
 class _MyHomePageState extends State<MyHomePage> {
   Color _begin = Colors.green;
   Color _end = Colors.orange;
   double _gap = 0.25;
-
-  List<EntryColor> _computeColors() {
-    List<EntryColor> _colors = [];
-
-    _colors.add(
-      EntryColor(
-        key: 'Color.lerp(begin, end, 0.0)',
-        value: Color.lerp(_begin, _end, 0),
-      ),
-    );
-
-    for (var _t = _gap; _t < 1; _t = formatDouble(_t + _gap)) {
-      _colors.add(
-        EntryColor(
-          key: 'Color.lerp(begin, end, $_t)',
-          value: Color.lerp(_begin, _end, _t),
-        ),
-      );
-    }
-
-    _colors.add(
-      EntryColor(
-        key: 'Color.lerp(begin, end, 1.0)',
-        value: Color.lerp(_begin, _end, 1),
-      ),
-    );
-
-    return _colors;
-  }
 
   void _onChangeGap(double value) {
     if (value == 0) {
@@ -92,8 +58,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final _colors = _computeColors();
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -123,25 +87,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 value: _gap,
               ),
             ),
-            Expanded(
-              child: ListView.separated(
-                itemCount: _colors.length,
-                padding: EdgeInsets.only(bottom: 8),
-                separatorBuilder: (_, __) => SizedBox(height: 8),
-                itemBuilder: (_, index) {
-                  final _color = _colors[index];
-
-                  return Container(
-                    color: _color.value,
-                    child: ListTile(
-                      title: ColorText(
-                        color: _color.value,
-                        text: '${_color.key} → ${_color.value}',
-                      ),
-                    ),
-                  );
-                },
-              ),
+            ColorLerps(
+              begin: _begin,
+              end: _end,
+              gap: _gap,
             ),
           ],
         ),
---
+++ lib/widgets/ColorLerps.dart
@@ -0,0 +1,78 @@
+import 'package:color_lerp_visualizer/models/EntryColor.dart';
+import 'package:color_lerp_visualizer/widgets/ColorText.dart';
+import 'package:flutter/material.dart';
+
+double formatDouble(double value) {
+  return double.parse(value.toStringAsFixed(2));
+}
+
+class ColorLerps extends StatelessWidget {
+  const ColorLerps({
+    Key key,
+    @required this.begin,
+    @required this.end,
+    @required this.gap,
+  })  : assert(begin != null),
+        assert(end != null),
+        assert(gap != null),
+        super(key: key);
+
+  final Color begin;
+  final Color end;
+  final double gap;
+
+  List<EntryColor> _computeColors() {
+    List<EntryColor> _colors = [];
+
+    _colors.add(
+      EntryColor(
+        key: 'Color.lerp(begin, end, 0.0)',
+        value: Color.lerp(begin, end, 0),
+      ),
+    );
+
+    for (var _t = gap; _t < 1; _t = formatDouble(_t + gap)) {
+      _colors.add(
+        EntryColor(
+          key: 'Color.lerp(begin, end, $_t)',
+          value: Color.lerp(begin, end, _t),
+        ),
+      );
+    }
+
+    _colors.add(
+      EntryColor(
+        key: 'Color.lerp(begin, end, 1.0)',
+        value: Color.lerp(begin, end, 1),
+      ),
+    );
+
+    return _colors;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _colors = _computeColors();
+
+    return Expanded(
+      child: ListView.separated(
+        itemCount: _colors.length,
+        padding: EdgeInsets.only(bottom: 8),
+        separatorBuilder: (_, __) => SizedBox(height: 8),
+        itemBuilder: (_, index) {
+          final _color = _colors[index];
+
+          return Container(
+            color: _color.value,
+            child: ListTile(
+              title: ColorText(
+                color: _color.value,
+                text: '${_color.key} → ${_color.value}',
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
