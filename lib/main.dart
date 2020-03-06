e5a1592565685de1785243cc95bbc1b7d4d71653
Arthur Denner <arthurdenner7@gmail.com>
2020-03-06 10:57:53 +0100

    refactor: create page class for HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:color_lerp_visualizer/widgets/ColorButton.dart';
-import 'package:color_lerp_visualizer/widgets/ColorLerps.dart';
+import 'package:color_lerp_visualizer/pages/HomePage.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -21,80 +20,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: MyHomePage(title: 'Color.lerp visualizer'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Color _begin = Colors.green;
-  Color _end = Colors.orange;
-  double _gap = 0.25;
-
-  void _onChangeGap(double value) {
-    if (value == 0) {
-      return;
-    }
-
-    setState(() => _gap = value);
-  }
-
-  void _changeBeginColor(Color value) {
-    setState(() => _begin = value);
-  }
-
-  void _changeEndColor(Color value) {
-    setState(() => _end = value);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 8),
-            ColorButton(
-              color: _begin,
-              onColorChanged: _changeBeginColor,
-              text: 'begin: $_begin',
-            ),
-            SizedBox(height: 8),
-            ColorButton(
-              color: _end,
-              onColorChanged: _changeEndColor,
-              text: 'end: $_end',
-            ),
-            SizedBox(height: 8),
-            Padding(
-              padding: EdgeInsets.only(top: 0),
-              child: Slider.adaptive(
-                divisions: 100,
-                label: '$_gap',
-                onChanged: _onChangeGap,
-                value: _gap,
-              ),
-            ),
-            ColorLerps(
-              begin: _begin,
-              end: _end,
-              gap: _gap,
-            ),
-          ],
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,75 @@
+import 'package:color_lerp_visualizer/widgets/ColorButton.dart';
+import 'package:color_lerp_visualizer/widgets/ColorLerps.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color _begin = Colors.green;
+  Color _end = Colors.orange;
+  double _gap = 0.25;
+
+  void _onChangeGap(double value) {
+    if (value == 0) {
+      return;
+    }
+
+    setState(() => _gap = value);
+  }
+
+  void _changeBeginColor(Color value) {
+    setState(() => _begin = value);
+  }
+
+  void _changeEndColor(Color value) {
+    setState(() => _end = value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Color.lerp visualizer'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              ColorButton(
+                color: _begin,
+                onColorChanged: _changeBeginColor,
+                text: 'begin: $_begin',
+              ),
+              SizedBox(height: 8),
+              ColorButton(
+                color: _end,
+                onColorChanged: _changeEndColor,
+                text: 'end: $_end',
+              ),
+              SizedBox(height: 8),
+              Padding(
+                padding: EdgeInsets.only(top: 0),
+                child: Slider.adaptive(
+                  divisions: 100,
+                  label: '$_gap',
+                  onChanged: _onChangeGap,
+                  value: _gap,
+                ),
+              ),
+              ColorLerps(
+                begin: _begin,
+                end: _end,
+                gap: _gap,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
