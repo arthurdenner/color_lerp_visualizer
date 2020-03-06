73fb16d27e6c5d322683a95a182c88e627c2fbcc
Arthur Denner <arthurdenner7@gmail.com>
2020-03-06 09:09:34 +0100

    feat: add list with colors from lerp example

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+class EntryColor {
+  EntryColor({
+    @required this.key,
+    @required this.value,
+  });
+
+  final String key;
+  final Color value;
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Color.lerp visualizer',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(
+          color: Colors.black54,
+        ),
+        primarySwatch: Colors.grey,
+        primaryTextTheme: TextTheme(
+          headline6: TextStyle(
+            color: Colors.white,
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Color.lerp visualizer'),
     );
   }
 }
@@ -25,11 +44,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Color _begin = Colors.green;
+  Color _end = Colors.orange;
+  double _gap = .1;
+  List<EntryColor> _colors = [];
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _computeColors();
+  }
+
+  void _computeColors() {
+    List<EntryColor> _newColors = [];
+
+    for (var i = 0.0; i <= 1; i += _gap) {
+      final _t = i.toStringAsFixed(2);
+
+      _newColors.add(
+        EntryColor(
+          key: 'Color.lerp(begin, end, $_t)',
+          value: Color.lerp(_begin, _end, double.parse(_t)),
+        ),
+      );
+    }
+
     setState(() {
-      _counter++;
+      _colors = _newColors;
     });
   }
 
@@ -40,23 +81,21 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: ListView.separated(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          itemCount: _colors.length,
+          separatorBuilder: (_, __) => SizedBox(height: 8),
+          itemBuilder: (_, index) {
+            final _color = _colors[index];
+
+            return Container(
+              color: _color.value,
+              child: ListTile(
+                title: Text('${_color.key} → ${_color.value}'),
+              ),
+            );
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
